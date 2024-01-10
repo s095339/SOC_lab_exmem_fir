443a2537d220de992e657fe35a299e541dae7a62
s095339 <s095339@gmail.com>
2024-01-10 04:51:32 -0500

    [modify] modify linker script to load fir data to sdram

--- testbench/counter_la_fir/fir.c
+++ testbench/counter_la_fir/fir.c
@@ -10,27 +10,9 @@ void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
 }
 
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
-	int data_ram_start_ptr = 0;
+	//initfir();
 	for(int i=0;i<N;i++){
-		int ss_data = inputsignal[i]; // receive data from axi-stream
-		inputbuffer[data_ram_start_ptr] = ss_data;// store input in data_ram
-
-		//fir-calculation===========
-		int psum_buff = 0;
-		for(int j=0;j<N;j++){
-
-			int data_ram_addr = data_ram_start_ptr-j;
-			if(data_ram_addr<0)data_ram_addr = N - data_ram_addr;
-
-			psum_buff += inputbuffer[data_ram_addr] * taps[j];  
-		}
-		//============================
-		data_ram_start_ptr++;
-		if(data_ram_start_ptr>=N)
-			data_ram_start_ptr = data_ram_start_ptr-N;
-		
-		outputsignal[i] = psum_buff;
-		//=======================
+		outputsignal[i] = inputsignal[i];
 	}
 	return outputsignal;
 }
--- testbench/counter_la_fir/fir.h
+++ testbench/counter_la_fir/fir.h
@@ -3,8 +3,9 @@
 
 #define N 11
 
+//#define RCV_SDRAM_INDATA __attribute__((section(".indata")))
 int taps[N] = {0,-10,-9,23,56,63,56,23,-9,-10,0};
 int inputbuffer[N];
-int inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,11};
+volatile int __attribute__((section(".indata")))inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,918};
 int outputsignal[N];
 #endif
